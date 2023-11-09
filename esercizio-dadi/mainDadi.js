d28cc326e01a99c67293e60893f39f3f1a8cf6ea
DavideCiammetti <davideciammetti97@gmail.com>
2023-11-09 16:25:39 +0100

    feat: gioco dei dadi

--- esercizio-dadi/mainDadi.js
+++ esercizio-dadi/mainDadi.js
@@ -2,8 +2,27 @@
 const inputInizioGioco = parseInt(prompt('se vuoi iniziare il gioco premi 1'));
 
 let start = 0;
+// variabili controllo numeri random
 let min = 1;
 let max = 6;
-let random = Math.floor(Math.random() * (max - min + 1)) + min;
 
-console.log(random);
+let userPlayer = Math.floor(Math.random() * (max - min + 1)) + min;
+let pcPlayer = Math.floor(Math.random() * (max - min + 1)) + min;;
+
+if(inputInizioGioco == 1){
+    // numero utente
+    console.log("il numero dell'utente è:" + ' ' + userPlayer);
+    // numero computer
+    console.log("il numero del computer è:" + ' ' + pcPlayer);
+
+    if(userPlayer > pcPlayer){
+        console.log("l'utente ha vinto e il computer è stato sconfitto");
+    }else if(userPlayer < pcPlayer){
+        console.log("il compuer ha vinto e l'utente è stato sconfitto ");
+    }else{
+        console.log("il risultato è pari");
+    }
+
+}else{
+    console.log('fine del gioco');
+}
